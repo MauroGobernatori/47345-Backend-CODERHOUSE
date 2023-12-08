414804b5178cacbc9fd4c1ef0a907dfcf630b97b
MauroGobernatori <mauro.gobernatori@gmail.com>
2023-12-07 21:51:42 -0300

    Entregable 05 - edit 1

--- Entregable05-MauroGobernatori/src/routes/userRouter.js
+++ Entregable05-MauroGobernatori/src/routes/userRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import UserController from "../controllers/userController.js";
 const controller = new UserController();
+import { validateLogin } from '../middlewares/validateLogin.js';
 
 const router = Router();
 
 router.post("/register", controller.register);
-router.post("/login", controller.login);
+router.post("/login", validateLogin, controller.login);
 router.post('/logout', controller.logout);
 
 export default router;
